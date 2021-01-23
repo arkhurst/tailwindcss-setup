e7e5953dd4f312374a7daf2600f6e2f4b419684d
arkhurst <fiiarkhurst@gmail.com>
2021-01-23 13:07:03 +0000

    everything works now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="font-bold text-5xl">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
